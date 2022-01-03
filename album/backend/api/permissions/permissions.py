45d4ae96afc6ddd23d4d9a4263358aa0e42f7c12
Escalop <germes.36@yandex.ru>
2022-01-03 16:58:44 -0500

    Users are not seeing each others albums

--- album/backend/api/permissions/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-        return obj.owner.id == request.user.id
--- album/backend/api/views/views.py
+++ album/backend/api/views/views.py
@@ -1,14 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models import Count, Q
+from django.db.models import Count
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.filters import OrderingFilter
+import rest_framework.filters
 from rest_framework.response import Response
-from ..permissions.permissions import IsOwnerOrReadOnly
 
 from ..models.api_page import AlbumImage, Album
 from ..serializers.serializers import AlbumSerializer, ImageSerializer
-from rest_framework import generics, status
+from rest_framework import generics, status, filters
 from user.models import User
 from user.serializers import UserSerializer
 
@@ -30,27 +29,25 @@ class RegisterUserView(CreateAPIView):
             return Response(data)
 
 
+class UserFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(owner=request.user)
+
+
 class AlbumList(generics.ListCreateAPIView):
-    queryset = Album.objects.all()
-    #queryset = Album.objects.annotate(num_items=Count('photos__pic'))
+    queryset = Album.objects.annotate(num_items=Count('photos__pic'))
     serializer_class = AlbumSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    filter_backends = [OrderingFilter]
+    filter_backends = [rest_framework.filters.OrderingFilter, UserFilterBackend]
     ordering_fields = ['created', 'num_items']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-    def get_queryset(self):
-        user = self.request.user
-        return Album.objects.annotate(num_items=Count('photos__pic', filter=Q(owner=user)))
 
 
 class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
@@ -60,8 +57,7 @@ class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
 class ImageList(generics.ListCreateAPIView):
     queryset = AlbumImage.objects.all()
     serializer_class = ImageSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filter_backends = [rest_framework.filters.OrderingFilter, DjangoFilterBackend, UserFilterBackend]
     ordering_fields = ['created', 'album']
     filterset_fields = ['tags', 'album']
 
@@ -76,7 +72,6 @@ class ImageList(generics.ListCreateAPIView):
 class ImageDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = AlbumImage.objects.all()
     serializer_class = ImageSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
